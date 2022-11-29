f2309c18440506dd8b024b9e5496b48892ca6454
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-11-28 22:42:39 -0500

    added skills section

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -1,7 +1,30 @@
+import { TECH_STACK } from '../../content/skills'
+import './TechStack.css'
+
 const TechStack = () => {
   return (
-    <div className="mt-24">
+    <div className="TechStack">
       <h3 className="SectionHeader">Tech Stack</h3>
+      <ul className="SkillGrid">
+        {TECH_STACK.map((skillset, sectionIndex) => {
+          return (
+            <li key={sectionIndex} className="flex flex-col items-center">
+              <h4 className="font-fira-code text-accent-1 text-center mb-1">
+                {skillset.section}
+              </h4>
+              <div className="SkillBox">
+                <ul className="text-sm">
+                  {skillset.skills.map((skill, skillIndex) => (
+                    <li key={skillIndex} className="Skill">
+                      {skill}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
---
+++ src/content/skills.ts
@@ -0,0 +1,34 @@
+export const TECH_STACK = [
+  {
+    section: 'JavaScript',
+    skills: ['ES6+', 'TypeScript', 'Create React App', 'Gatsby', 'jQuery'],
+  },
+  {
+    section: 'Styling',
+    skills: ['Tailwind', 'MUI', 'Styled Components', 'SCSS', 'CSS3'],
+  },
+  {
+    section: 'State Management',
+    skills: ['Recoil', 'Apollo Client'],
+  },
+  {
+    section: 'Data',
+    skills: ['REST', 'GraphQL', 'MongoDB', 'Firebase'],
+  },
+  {
+    section: 'Server-Side',
+    skills: ['Node.js', 'Express', 'Apollo Server'],
+  },
+  {
+    section: 'Content Management',
+    skills: ['Strapi', 'WordPress', 'Shopify'],
+  },
+  {
+    section: 'Testing',
+    skills: ['React Testing Library', 'Cypress', 'Jest', 'TestCafe'],
+  },
+  {
+    section: `Other`,
+    skills: ['Storybook', 'Figma', 'Docker', 'PHP', 'Liquid'],
+  },
+]
