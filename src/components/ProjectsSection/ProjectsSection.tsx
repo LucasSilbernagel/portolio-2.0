d10a6111989704d5996531b8ea31ee2661f79622
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2022-12-01 22:42:35 -0500

    queried project images

--- src/components/ProjectsSection/ProjectsSection.tsx
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,11 +1,64 @@
+import { GatsbyImage } from 'gatsby-plugin-image'
+import { PROJECTS } from '../../content/projects'
 import './ProjectsSection.css'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const ProjectsSection = () => {
+  const projectImageNames = PROJECTS.map((project) => project.imageFileName)
+
+  const imageQuery = useStaticQuery(query)
+
+  const projectImages = imageQuery.allImageSharp.nodes
+    .filter((node: { original: { src: string } }) =>
+      projectImageNames.includes(node.original.src.split('/')[2].split('-')[0])
+    )
+    .map((node: { gatsbyImageData: string; original: { src: string } }) => {
+      return {
+        gatsbyImageData: node.gatsbyImageData,
+        imageName: node.original.src.split('/')[2].split('-')[0],
+      }
+    })
+
   return (
     <div className="ProjectsSection" id="projects-section">
       <h3 className="SectionHeader">Projects</h3>
+      <ul>
+        {PROJECTS.map((project) => {
+          return (
+            <li key={project.github}>
+              <div>
+                <div className="w-[580px] h-[363px]">
+                  <GatsbyImage
+                    image={
+                      projectImages.filter(
+                        (projectImage: { imageName: string }) =>
+                          projectImage.imageName === project.imageFileName
+                      )[0].gatsbyImageData
+                    }
+                    alt={project.name}
+                    className="w-full h-full object-contain"
+                  />
+                </div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
+
+export const query = graphql`
+  query Images {
+    allImageSharp {
+      nodes {
+        gatsbyImageData
+        original {
+          src
+        }
+      }
+    }
+  }
+`
 
 export default ProjectsSection
--- src/content/projects.ts
+++ src/content/projects.ts
@@ -1,16 +1,14 @@
 export const PROJECTS = [
   {
-    id: 1,
     name: 'Tasty',
     description:
       'A multi-page website of tasty recipes, inspired by Delish.com. Content is saved and provided by Strapi CMS, with images and video hosted in Cloudinary.',
     techStack: ['Gatsby', 'React', 'TypeScript', 'Strapi', 'Tailwind'],
     github: 'https://github.com/LucasSilbernagel/tasty',
     liveLink: 'https://symphonious-croquembouche-c45916.netlify.app/',
-    image: '../images/tasty.png',
+    imageFileName: 'tasty',
   },
   {
-    id: 2,
     name: 'Been There, Done That',
     description:
       'A travel destination log. City and tourist attraction data is provided by the Geoapify Places API and converted to GraphQL with Apollo Server and Apollo Client.',
@@ -26,10 +24,9 @@ export const PROJECTS = [
     ],
     github: 'https://github.com/LucasSilbernagel/bt-dt',
     liveLink: 'https://shiny-gecko-cc92d2.netlify.app/',
-    image: '../images/bt-dt.png',
+    imageFileName: 'btdt',
   },
   {
-    id: 3,
     name: 'Keep Clone',
     description:
       'A clone of Google Keep built with the MERN stack. Replicates the web app on larger screens and the mobile app on smaller screens. Includes Google authentication and unit tests. Components are documented with Storybook.',
@@ -44,10 +41,9 @@ export const PROJECTS = [
     ],
     github: 'https://github.com/LucasSilbernagel/keep-clone',
     liveLink: 'https://glowing-moonbeam-f2cd62.netlify.app/',
-    image: '../images/keep-clone.png',
+    imageFileName: 'keepClone',
   },
   {
-    id: 4,
     name: 'Weather Dash',
     description:
       'A simple weather dashboard build with React, TypeScript, and a REST API. Search for and view a five day weather forecast for any city. Includes unit tests (Jest, React Testing Library), e2e tests (Cypress), and state management (Recoil).',
@@ -62,26 +58,24 @@ export const PROJECTS = [
     ],
     github: 'https://github.com/LucasSilbernagel/weather-dash',
     liveLink: 'https://lucassilbernagel.github.io/weather-dash/',
-    image: '../images/weather-dash.png',
+    imageFileName: 'weatherDash',
   },
   {
-    id: 5,
     name: 'Word Game DB',
     description:
       'A REST API that is intended to be used by anyone who would like to practice their coding skills by building a word game with an API.',
     techStack: ['MongoDB', 'Express', 'React', 'Node.js', 'Styled Components'],
     github: 'https://github.com/LucasSilbernagel/wordGameDB',
     liveLink: 'https://www.wordgamedb.com/',
-    image: '../images/word-game-db.png',
+    imageFileName: 'wordGameDB',
   },
   {
-    id: 6,
     name: 'bestFlix',
     description:
       'The best movies of all time, as voted by you! Search for and nominate up to four movies, and vote for your favourites on the list of nominees.',
     techStack: ['React', 'Firebase', 'REST API', 'SCSS'],
     github: 'https://github.com/LucasSilbernagel/bestFlix',
     liveLink: 'https://lucassilbernagel.github.io/bestFlix/',
-    image: '../images/bestFlix.png',
+    imageFileName: 'bestFlix',
   },
 ]
