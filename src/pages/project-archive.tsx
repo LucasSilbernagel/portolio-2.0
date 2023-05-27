978986a482610eded66278b3415f6caf0fec25f4
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-05-26 22:47:49 -0400

    added project-archive page

---
+++ src/pages/project-archive.tsx
@@ -0,0 +1,17 @@
+import Layout from '../components/Layout'
+
+const ProjectArchivePage = () => {
+  return (
+    <Layout
+      isHomePage={false}
+      pageTitle="Project Archive"
+      pageRoute="/project-archive"
+    >
+      <div>
+        <h1>Project Archive</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default ProjectArchivePage
