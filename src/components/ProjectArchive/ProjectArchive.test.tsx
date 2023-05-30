51904e9c6fa92224980b9f903592f54bc2451752
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-05-29 21:53:12 -0400

    set up infinite scroll for project archive

--- src/components/ProjectArchive/ProjectArchive.test.tsx
+++ src/components/ProjectArchive/ProjectArchive.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import ProjectArchive from './ProjectArchive'
 import { PROJECTS } from '../../content/projects'
 
@@ -13,30 +13,32 @@ describe('ProjectArchive', () => {
       )
     ).toBeInTheDocument()
     PROJECTS.forEach((project, index) => {
-      expect(screen.getByTestId(`project-year-${index}`)).toBeInTheDocument()
-      expect(screen.getByTestId(`project-year-${index}`)).toHaveTextContent(
-        project.year
-      )
-      expect(
-        screen.getByTestId(`github-project-link-${index}`)
-      ).toBeInTheDocument()
-      expect(
-        screen.getByTestId(`github-project-link-${index}`)
-      ).toHaveAttribute('href', project.github)
-      expect(
-        screen.getByTestId(`external-project-link-${index}`)
-      ).toBeInTheDocument()
-      expect(
-        screen.getByTestId(`external-project-link-${index}`)
-      ).toHaveAttribute('href', project.liveLink)
-      expect(screen.getByText(project.name)).toBeInTheDocument()
-      expect(screen.getByText(project.name)).toHaveAttribute(
-        'href',
-        project.liveLink
-      )
-      expect(screen.getByText(project.description)).toBeInTheDocument()
-      project.techStack.forEach((skill) => {
-        expect(screen.getAllByText(skill)[0]).toBeInTheDocument()
+      waitFor(() => {
+        expect(screen.getByTestId(`project-year-${index}`)).toBeInTheDocument()
+        expect(screen.getByTestId(`project-year-${index}`)).toHaveTextContent(
+          project.year
+        )
+        expect(
+          screen.getByTestId(`github-project-link-${index}`)
+        ).toBeInTheDocument()
+        expect(
+          screen.getByTestId(`github-project-link-${index}`)
+        ).toHaveAttribute('href', project.github)
+        expect(
+          screen.getByTestId(`external-project-link-${index}`)
+        ).toBeInTheDocument()
+        expect(
+          screen.getByTestId(`external-project-link-${index}`)
+        ).toHaveAttribute('href', project.liveLink)
+        expect(screen.getByText(project.name)).toBeInTheDocument()
+        expect(screen.getByText(project.name)).toHaveAttribute(
+          'href',
+          project.liveLink
+        )
+        expect(screen.getByText(project.description)).toBeInTheDocument()
+        project.techStack.forEach((skill) => {
+          expect(screen.getAllByText(skill)[0]).toBeInTheDocument()
+        })
       })
     })
   })
--- src/components/ProjectArchive/ProjectArchive.tsx
+++ src/components/ProjectArchive/ProjectArchive.tsx
@@ -2,8 +2,128 @@ import { PROJECTS } from '../../content/projects'
 import './ProjectArchive.css'
 import { FaGithub, FaExternalLinkAlt } from 'react-icons/fa'
 import { AnimationOnScroll } from 'react-animation-on-scroll'
+import InfiniteScroll from 'react-infinite-scroller'
+import { useState } from 'react'
+
+interface IProject {
+  name: string
+  year: string
+  description: string
+  techStack: string[]
+  github: string
+  liveLink: string
+  imageFileName?: string
+}
 
 const ProjectArchive = () => {
+  const getItemsPerPage = (number: number) => {
+    const divisors: number[] = []
+    for (let divisor = 1; divisor <= number; divisor++) {
+      if (number % divisor === 0) {
+        divisors.push(divisor)
+      }
+    }
+    const idealDivisors = divisors.filter(
+      (divisor) => divisor > 2 && divisor < 13
+    )
+    if (idealDivisors.length > 0) {
+      return idealDivisors[0]
+    } else return divisors.sort((a, b) => b - a)[0]
+  }
+
+  const itemsPerPage = getItemsPerPage(PROJECTS.length)
+  const [hasMore, setHasMore] = useState<boolean>(true)
+  const [records, setRecords] = useState<number>(itemsPerPage)
+
+  const loadMore = () => {
+    if (records === PROJECTS.length) {
+      setHasMore(false)
+    } else {
+      setTimeout(() => {
+        setRecords(records + itemsPerPage)
+      }, 1000)
+    }
+  }
+
+  const showProjects = (projects: IProject[]) => {
+    const items = []
+    for (let i = 0; i < records; i++) {
+      items.push(
+        <li key={`${projects[i].github}`}>
+          <div className="Project">
+            <div className="flex justify-between mb-4">
+              <div>
+                <h3
+                  className="AccentFont text-accent-1"
+                  data-testid={`project-year-${i}`}
+                >
+                  {projects[i].year}
+                </h3>
+              </div>
+              <div className="flex gap-8 text-lg justify-end">
+                <div>
+                  <a
+                    href={projects[i].github}
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label={`GitHub repository for ${projects[i].name}`}
+                    className="IconLink"
+                    data-testid={`github-project-link-${i}`}
+                  >
+                    <FaGithub />
+                  </a>
+                </div>
+                <div>
+                  <a
+                    href={projects[i].liveLink}
+                    target="_blank"
+                    rel="noreferrer"
+                    aria-label={`live link for ${projects[i].name}`}
+                    className="IconLink"
+                    data-testid={`external-project-link-${i}`}
+                  >
+                    <FaExternalLinkAlt />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <h4>
+              <a
+                href={projects[i].liveLink}
+                target="_blank"
+                rel="noreferrer"
+                aria-label={`live link for ${projects[i].name}`}
+                className="ProjectName"
+              >
+                {projects[i].name}
+              </a>
+            </h4>
+            <div className=" text-sm text-slate-2 tracking-wide my-6">
+              <p>{projects[i].description}</p>
+            </div>
+            <div className="text-slate-2 text-sm font-fira-code mb-6">
+              <ul className="flex flex-wrap gap-x-4 gap-y-2">
+                {projects[i].techStack.map((skill, skillIndex) => {
+                  return (
+                    <li
+                      key={`${projects[i].github}-${skill.replace(
+                        / /g,
+                        ''
+                      )}-${skillIndex}-mobile`}
+                    >
+                      {skill}
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </li>
+      )
+    }
+    return items
+  }
+
   return (
     <AnimationOnScroll animateIn="animate__fadeIn" animateOnce={true}>
       <div
@@ -16,82 +136,24 @@ const ProjectArchive = () => {
           Educational, freelance, and side projects I&apos;ve completed over the
           years
         </h2>
-        <ul>
-          {PROJECTS.map((project, index) => {
-            return (
-              <li key={`${project.github}`}>
-                <div className="Project">
-                  <div className="flex justify-between mb-4">
-                    <div>
-                      <h3
-                        className="AccentFont text-accent-1"
-                        data-testid={`project-year-${index}`}
-                      >
-                        {project.year}
-                      </h3>
-                    </div>
-                    <div className="flex gap-8 text-lg justify-end">
-                      <div>
-                        <a
-                          href={project.github}
-                          target="_blank"
-                          rel="noreferrer"
-                          aria-label={`GitHub repository for ${project.name}`}
-                          className="IconLink"
-                          data-testid={`github-project-link-${index}`}
-                        >
-                          <FaGithub />
-                        </a>
-                      </div>
-                      <div>
-                        <a
-                          href={project.liveLink}
-                          target="_blank"
-                          rel="noreferrer"
-                          aria-label={`live link for ${project.name}`}
-                          className="IconLink"
-                          data-testid={`external-project-link-${index}`}
-                        >
-                          <FaExternalLinkAlt />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <h4>
-                    <a
-                      href={project.liveLink}
-                      target="_blank"
-                      rel="noreferrer"
-                      aria-label={`live link for ${project.name}`}
-                      className="ProjectName"
-                    >
-                      {project.name}
-                    </a>
-                  </h4>
-                  <div className=" text-sm text-slate-2 tracking-wide my-6">
-                    <p>{project.description}</p>
-                  </div>
-                  <div className="text-slate-2 text-sm font-fira-code mb-6">
-                    <ul className="flex flex-wrap gap-x-4 gap-y-2">
-                      {project.techStack.map((skill, skillIndex) => {
-                        return (
-                          <li
-                            key={`${project.github}-${skill.replace(
-                              / /g,
-                              ''
-                            )}-${skillIndex}-mobile`}
-                          >
-                            {skill}
-                          </li>
-                        )
-                      })}
-                    </ul>
-                  </div>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+        <InfiniteScroll
+          element="ul"
+          pageStart={0}
+          loadMore={loadMore}
+          hasMore={hasMore}
+          loader={
+            <div className="w-full flex justify-center" key="loader">
+              <div className="Loader">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          }
+        >
+          {showProjects(PROJECTS)}
+        </InfiniteScroll>
       </div>
     </AnimationOnScroll>
   )
