a588f4fe11969445b0206f0fc40b8c515810b115
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-03-14 16:23:05 -0400

    content update

--- src/content/experience.ts
+++ src/content/experience.ts
@@ -6,7 +6,7 @@ export const EXPERIENCE = [
     timeframe: '2023-01 — present',
     timeframeLong: 'January 2023 to present',
     accomplishments: [
-      `Enhanced the Xanadu.ai website by implementing server-side rendering, image optimization, and several new pages. Leveraged Shopify APIs to integrate the Xanadu merchandise shop seamlessly into the website.`,
+      `Enhanced the Xanadu.ai website by implementing server-side rendering, image optimization, and several new pages. Leveraged Shopify APIs to seamlessly integrate the Xanadu merchandise shop into the website.`,
       `Drove the front-end work for a refresh of the quantum hackathon software challenge dashboard.`,
       `Migrated the PennyLane.ai website from a Python templating system to a modern development framework using Gatsby, React, TypeScript, and Tailwind.`,
       `Led front-end development of the Generic Datasets project for visualizing and interacting with quantum datasets (scoping, requirements gathering, and implementation).`,
