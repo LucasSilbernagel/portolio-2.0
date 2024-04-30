76bc90eee971a325ae6ebe2d79a2302b7fc1d6a0
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-04-29 21:49:26 -0400

    updated tests and other small things

--- src/components/SideBars/SideBars.test.tsx
+++ src/components/SideBars/SideBars.test.tsx
@@ -1,5 +1,7 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import SideBars from './SideBars'
+
+window.prompt = jest.fn()
 
 describe('SideBars', () => {
   test('renders correctly', () => {
@@ -7,5 +9,7 @@ describe('SideBars', () => {
     expect(screen.getByTestId('social-links-sidebar')).toBeInTheDocument()
     expect(screen.getByTestId('email-sidebar')).toBeInTheDocument()
     expect(screen.getByText('hello@lucassilbernagel.com')).toBeInTheDocument()
+    fireEvent.click(screen.getByText('hello@lucassilbernagel.com'))
+    expect(screen.getByText('Copied!')).toBeVisible()
   })
 })
