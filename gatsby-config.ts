cd99c69f68b2a3fcc65176bd132921d5f1000fa4
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2024-05-12 22:49:39 -0400

    updated sitemap config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,17 +1,19 @@
-import type { GatsbyConfig } from 'gatsby'
+import type { GatsbyConfig, Page } from 'gatsby'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+const siteMetadata = {
+  title: `Lucas Silbernagel | Software Developer`,
+  description: `Personal website and software development portfolio for Lucas Silbernagel`,
+  image: '/portfolio.png',
+  siteUrl: `https://lucassilbernagel.com`,
+}
+
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Lucas Silbernagel | Software Developer`,
-    description: `Personal website and software development portfolio for Lucas Silbernagel`,
-    image: '/portfolio.png',
-    siteUrl: `https://lucassilbernagel.com`,
-  },
+  siteMetadata: siteMetadata,
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
@@ -19,7 +21,36 @@ const config: GatsbyConfig = {
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-smoothscroll',
-    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        query: `
+        {
+          allSitePage {
+            nodes {
+              path
+            }
+          }
+        }
+      `,
+        resolveSiteUrl: () => siteMetadata.siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes: allPages },
+        }: {
+          allSitePage: { nodes: Page[] }
+        }) => {
+          return allPages.map((page: Page) => {
+            return { ...page }
+          })
+        },
+        serialize: ({ path }: { path: string; modifiedGmt: string }) => {
+          return {
+            url: path,
+            lastmod: new Date().toISOString(),
+          }
+        },
+      },
+    },
     'gatsby-plugin-postcss',
     {
       resolve: 'gatsby-plugin-html-attributes',
